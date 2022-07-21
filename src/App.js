5df475637ec7205652261b23bc45df3b03ac4b67
flex6699 <aniruddhabohraabc@gmail.com>
2022-07-21 11:49:47 +0530

    v1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import './App.css'
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+  return <div className="App">Hello</div>;
 }
 
-export default App
+export default App;
